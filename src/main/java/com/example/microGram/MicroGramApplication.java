9cc2d264d67994a2743286515eaedcf17a2f2ee9
Andrei Likhoded <liberty102and8131186@gmail.com>
2022-09-18 20:43:36 +0600

    de

---
+++ src/main/java/com/example/microGram/MicroGramApplication.java
@@ -0,0 +1,13 @@
+package com.example.microGram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroGramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroGramApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/microGram/MicroGramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.microGram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroGramApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
